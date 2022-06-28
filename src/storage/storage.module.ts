316fc08d8df58d05bf4419f98bd5ce44ffb5e379
Artem Abdullin <artemleelooai@gmail.com>
2022-06-29 00:14:53 +0300

    Added Config

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StorageService } from './storage.service';
 import { StorageController } from './storage.controller';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Storage } from './entities/storage.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Storage])],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forFeature([Storage])],
   controllers: [StorageController],
   providers: [StorageService],
 })
